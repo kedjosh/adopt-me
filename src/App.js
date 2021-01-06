8d0ee928f791fb2fb814aaf1ef817c2dc3801621
Kedar Joshi <kedar@Kedars-MBP.fios-router.home>
2021-01-06 14:42:17 -0500

    update

--- src/App.js
+++ src/App.js
@@ -6,7 +6,7 @@ import Details from "./Details";
 import ThemeContext from "./ThemeContext";
 
 const App = () => {
-  const themeHook = useState("yellow");
+  const themeHook = useState("black");
   return (
     <React.StrictMode>
       <ThemeContext.Provider value={themeHook}>
